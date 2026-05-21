c6c37b5163a1fae4b8c712f4bcb69823bfcfc4be
fhr129-cloud <rok7836@naver.com>
2026-05-21 17:17:00 +0900

    Create firebase-config.js

---
+++ src/js/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDd67rNSC-d2GzZaLAcT2XP8m9c_2K1gzo",
+  authDomain: "contract-renewal-e0c6d.firebaseapp.com",
+  projectId: "contract-renewal-e0c6d",
+  storageBucket: "contract-renewal-e0c6d.firebasestorage.app",
+  messagingSenderId: "977225940543",
+  appId: "1:977225940543:web:6080359213deca262bc28a"
+};
+
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
