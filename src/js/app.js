bd311d1b5b96c386657a899c4f6ebc489aae9aa0
fhr129-cloud <rok7836@naver.com>
2026-06-13 22:25:29 +0900

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -980,8 +980,9 @@ window.saveContract=async function(){
   };
   try{
     if(editingId){
-      var oldContract=contracts.find(function(x){ return x.id===editingId; });
+     var oldContract=contracts.find(function(x){ return x.id===editingId; });
       var oldName=oldContract?oldContract.name:'';
+      console.log('oldName:', oldName, '/ newName:', name, '/ 같음?', oldName===name);
       await updateContract(editingId,data);
       if(oldName && oldName !== name) {
         var supsToUpdate=supports.filter(function(s){ return s.bizName===oldName; });
