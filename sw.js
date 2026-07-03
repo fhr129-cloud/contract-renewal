50ceb40578ce66280f381bedbca98f5641a6da13
fhr129-cloud <rok7836@naver.com>
2026-07-03 20:57:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,3 @@
-const CACHE_NAME = 'fs-app-v3';
-const urlsToCache = ['/contract-renewal/'];
-
-self.addEventListener('install', function(e){
-  e.waitUntil(caches.open(CACHE_NAME).then(function(cache){ return cache.addAll(urlsToCache); }));
-});
 self.addEventListener('fetch', function(e){
-  e.respondWith(caches.match(e.request).then(function(response){ return response||fetch(e.request); }));
+  e.respondWith(fetch(e.request));
 });
