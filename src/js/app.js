0ab0c0ce66e49b1b61015ed11ab693dec225d1a6
fhr129-cloud <rok7836@naver.com>
2026-07-03 21:07:31 +0900

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1019,6 +1019,7 @@ function renderDashboard() {
   });
 recentUpdates.sort(function(a,b){ return b.updDate-a.updDate; });
   var recentEl=document.getElementById('recent-update-list');
+  console.log('recentUpdates 전체:', recentUpdates.length, recentUpdates.map(function(x){ return {name:x.c.name,addType:x.latest.addType}; }));
   var news=recentUpdates.filter(function(x){ return x.latest.addType==='new'&&!x.c.terminated; });
   var renewals=recentUpdates.filter(function(x){ return x.latest.addType==='renewal'; });
   var terminations=recentUpdates.filter(function(x){ return x.latest.addType==='terminate'; });
