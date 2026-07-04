3939f58bfd10c3e3b03eea2109e439089aecdea2
fhr129-cloud <rok7836@naver.com>
2026-07-04 13:50:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(names){
+      return Promise.all(names.map(function(name){ return caches.delete(name); }));
+    }).then(function(){
+      return self.clients.claim();
+    })
+  );
+});
 self.addEventListener('fetch', function(e){
   e.respondWith(fetch(e.request));
 });
